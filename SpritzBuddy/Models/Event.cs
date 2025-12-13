0800b62c693a00407a1bd8af224f5b7cd2db9d77
Marius 0G <mariusmanea.teo@gmail.com>
2025-12-14 01:23:12 +0200

    Reapply "Merge branch 'develop' of https://github.com/Marius0G/SpritzBuddy into develop"
    This reverts commit 0e108f33fb2be3b25cb18e18e9559e3787185744.

---
+++ SpritzBuddy/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpritzBuddy.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public required string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public DateTime EventDate { get; set; }
+        [MaxLength(250)]
+        public string? Location { get; set; }
+        public int OrganizerId { get; set; }
+        public virtual ApplicationUser Organizer { get; set; } = null!;
+        public virtual ICollection<EventParticipant> Participants { get; set; } = new List<EventParticipant>();
+    }
+}
---
+++ SpritzBuddy/Models/EventParticipant.cs
@@ -0,0 +1,11 @@
+﻿namespace SpritzBuddy.Models
+{
+    public class EventParticipant
+    {
+        public int UserId { get; set; }
+        public int EventId { get; set; }
+        public virtual ApplicationUser User { get; set; } = null!;
+        public virtual Event Event { get; set; } = null!;
+        public EventParticipantStatus Status { get; set; } = EventParticipantStatus.Interested;
+    }
+}
